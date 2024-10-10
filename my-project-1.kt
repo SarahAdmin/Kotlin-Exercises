38f03da960b83b65ff356235551c06e9989bb5ab
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-10-10 20:18:50 +0100

    Create my-project-1.kt

---
+++ my-project-1.kt
@@ -0,0 +1,5 @@
+fun main() {
+println("Hello World") // This is an output of the message
+println("This is my app in Kotlin") // This is my first time to code app in Kotlin
+println(20.00 / 100 * 10) /* This is the discount price */
+}
