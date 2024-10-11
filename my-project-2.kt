e80b10d110e6e5672a6dcde1cbdcb8ef2b48855b
SarahAdmin <82178834+SarahAdmin@users.noreply.github.com>
2024-10-11 13:07:03 +0100

    Create my-project-2.kt

---
+++ my-project-2.kt
@@ -0,0 +1,18 @@
+fun main() { 
+var name: String 
+  name = "Lee Cox" 
+val favouriteNumber: Int 
+  favouriteNumber = 8 
+val favouriteColour: String 
+  favouriteColour = "Blue" 
+  val favouriteSport: String 
+  favouriteSport = "Baseball"
+
+println(name)
+println(favouriteNumber)
+println(favouriteColour)
+println(favouriteSport)
+
+
+
+}
